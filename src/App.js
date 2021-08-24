ab8c5060870b3c4887c2491466eddca47c4fc0f3
Jason Moldovan <moldovanjason@gmail.com>
2021-08-24 18:31:56 -0400

    added stlying

--- src/App.js
+++ src/App.js
@@ -77,64 +77,71 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Employee Management System</h1>
       <div className="information">
-        <label>Name: </label>
+        <label className="Title">Enter Employee Information</label>
+        {/* <label>Name: </label> */}
         <input
           type="text"
           onChange={(e) => {
             setName(e.target.value);
           }}
+          placeholder="Name..."
         />
-        <label>Age: </label>
+        {/* <label>Age: </label> */}
         <input
           type="number"
           onChange={(e) => {
             setAge(e.target.value);
           }}
+          placeholder="Age..."
         />
-        <label>Country</label>
+        {/* <label>Country</label> */}
         <input
           type="text"
           onChange={(e) => {
             setCountry(e.target.value);
           }}
+          placeholder="Country..."
         />
-        <label>Position: </label>
+        {/* <label>Position: </label> */}
         <input
           type="text"
           onChange={(e) => {
             setPosition(e.target.value);
           }}
+          placeholder="Position..."
         />
-        <label>Yearly Wage: </label>
+        {/* <label>Yearly Wage: </label> */}
         <input
           type="number"
           onChange={(e) => {
             setWage(e.target.value);
           }}
+          placeholder="Wage..."
         />
         <button onClick={addEmployee}>Add Employee</button>
       </div>
-      <div className="line"></div>
+      {/* <div className="line"></div> */}
       <div className="employees">
         <button onClick={getEmployees}>Show Employees</button>
         {employeeList.map((val, key) => {
           return (
             <div className="employee">
               <h3>Name: {val.name}</h3>
-              <h4 />
+              {/* <h4 /> */}
               <h3>Age: {val.age}</h3>
-              <h4 />
+              {/* <h4 /> */}
               <h3>Position: {val.position}</h3>
-              <h4 />
+              {/* <h4 /> */}
               <h3>Country: {val.country}</h3>
-              <h4 />
+              {/* <h4 /> */}
               <h3>Wage: {val.wage}</h3>
               <div className="rightSide">
                 <input
                   className="employeePlaceholder"
                   type="number"
-                  placeholder="20000..."
+                  placeholder="New Wage..."
                   onChange={(e) => {
                     setNewWage(e.target.value);
                   }}
@@ -146,7 +153,14 @@ function App() {
                 >
                   Update Wage
                 </button>
-                <button className="deleteBtn" onClick={() => { deleteEmployee(val.id) }}>Delete Employee</button>
+                <button
+                  className="deleteBtn"
+                  onClick={() => {
+                    deleteEmployee(val.id);
+                  }}
+                >
+                  Delete Employee
+                </button>
               </div>
             </div>
           );
